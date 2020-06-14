b6c667ceef4377f9e1105ce91972c393bf3ff8b2
boluwatifeh <tifeasypeasy@gmail.com>
2020-06-14 16:40:15 +0100

    fixed c-style array declaration of parameters args

--- src/chapter5/InstantCreditCheck.java
+++ src/chapter5/InstantCreditCheck.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 
 public class InstantCreditCheck {
 
-    public static void main(String args []){
+    public static void main(String[] args){
 
         // initialize what we know
         int requiredSalary = 25000;
