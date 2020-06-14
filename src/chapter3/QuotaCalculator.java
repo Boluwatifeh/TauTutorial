5fd502feda12bfcfd55a88cf76eec53de1495280
boluwatifeh <tifeasypeasy@gmail.com>
2020-06-14 16:36:18 +0100

    fixed c-style array declaration of parameters args

--- src/chapter3/QuotaCalculator.java
+++ src/chapter3/QuotaCalculator.java
@@ -10,7 +10,7 @@ For those who don't, they are informed of how many sales they are short of
  */
 public class QuotaCalculator {
 
-    public static void main(String args[]){
+    public static void main(String[] args){
 
         int quota = 10;
 
