5c682df197a176184e593eb1e3bb72d64fe90f49
boluwatifeh <tifeasypeasy@gmail.com>
2020-06-14 16:35:31 +0100

    fixed indentations

--- src/chapter3/LoanQualifier.java
+++ src/chapter3/LoanQualifier.java
@@ -7,12 +7,10 @@ public class LoanQualifier {
     public static void main(String[] args){
 
         // Initialize what we know
-
         int salaryQuota = 30000;
         int yearsQuota = 2;
 
         // Get what we don't know
-
         System.out.println("Enter the amount of your salary earned: ");
         Scanner scanner = new Scanner(System.in);
         double salaryEarned = scanner.nextDouble();
@@ -21,7 +19,6 @@ public class LoanQualifier {
         int years = scanner.nextInt();
 
         // Make decision
-
         if(salaryEarned >= salaryQuota ) {
             if (years >= yearsQuota) {
                 System.out.println("You're qualified for the loan");
